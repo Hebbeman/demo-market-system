269083869d67c4a8476d9932098e95b355d450cc
Hebbeman <hebbeman@hobby>
2012-06-17 21:56:55 +0200

    Adding Junit dep

---
+++ src/test/java/main/TestMain.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TestMain extends TestCase {
+	
+	@Test
+	public void testSysout() {
+		System.out.println("Testing...");
+	}
+
+}
